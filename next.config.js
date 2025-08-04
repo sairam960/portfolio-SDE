a881b23de7ab6a1fabc203b1d3f6997a4f17bcee
Sairamnath Krishnan <59533563+sairam960@users.noreply.github.com>
2025-08-04 03:15:16 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,10 @@
-{/*
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   basePath: '/portfolio-SDE',
   assetPrefix: '/portfolio-SDE/',
-  images: {
-    unoptimized: true
-  }
-}
-
-*/}
-
-// next.config.js
-/** @type {import('next').NextConfig} */
-const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] // "portfolio‑SDE"
-const base = `/` + repo
-
-module.exports = {
-  basePath: base,           // path-based routing (Next.js ≥ 10)
-  assetPrefix: base + '/',  // prefix for static assets
-  trailingSlash: true,      // ensures index.html under each path
+  trailingSlash: true,
   images: {
     unoptimized: true
   }
