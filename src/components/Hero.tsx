353f933766feb42e1ec4c33eee5b0afb4faa0d84
Sairamnath Krishnan <59533563+sairam960@users.noreply.github.com>
2025-08-04 02:17:20 -0400

    picture

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -78,7 +78,7 @@ export default function Hero() {
                 }}
               >
                 <img
-                  src="/public/images/DSC_6481.jpg"
+                  src="/images/DSC_6481.jpg"
                   alt="Sai Krishnan"
                   style={{
                     width: "100%",
