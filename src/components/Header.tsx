16db161b4ee853cd37be8f51e2982d7185f855c9
Sairamnath Krishnan <59533563+sairam960@users.noreply.github.com>
2025-08-08 02:33:39 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -121,7 +121,7 @@ export default function Header() {
           style={{ scaleX }}
         />
         
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
           <nav className="header-nav-modern">
             {/* Logo/Brand with Magnetic Effect */}
             <motion.div 
